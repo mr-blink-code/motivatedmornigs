272c321070993395a706d8f1e955112c3ec98994
mr-blink-code <tmmahesh007@gmail.com>
2024-11-13 09:40:47 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['server/reply'], // Add the "server/reply" module here
+    },
+  },
 })
